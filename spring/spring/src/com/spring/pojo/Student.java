fc487f107d2b7f513da54569bdd4bb12ec538ff2
sanliman <1448553735@qq.com>
2019-09-27 23:14:42 +0800

    update project spring

---
+++ spring/spring/src/com/spring/pojo/Student.java
@@ -0,0 +1,20 @@
+package com.spring.pojo;
+
+public class Student {
+	private int sId;
+	private String sname;
+	public int getsId() {
+		return sId;
+	}
+	public void setsId(int sId) {
+		this.sId = sId;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	
+
+}
